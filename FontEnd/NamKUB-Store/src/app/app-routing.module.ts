9cf72a7deaa9d57128b13c68f605c4b933735f98
Nantanatorn Suetrong <Nantanatorn.s@ku.th>
2024-09-03 01:06:53 +0700

    2nd commit

--- FontEnd/NamKUB-Store/src/app/app-routing.module.ts
+++ FontEnd/NamKUB-Store/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './User/user-login/user-login.component';
+import { UserRegComponent } from './User/user-reg/user-reg.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: UserLoginComponent},
+  {path: 'register',component: UserRegComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FontEnd/NamKUB-Store/src/app/app.module.ts
+++ FontEnd/NamKUB-Store/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './User/user-login/user-login.component';
+import { UserRegComponent } from './User/user-reg/user-reg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    UserRegComponent
   ],
   imports: [
     BrowserModule,
