e13ec5cb83c6a0555846bc2867b12468cc8996e5
nubnibjung <gidjung120347@gmail.com>
2024-09-14 16:15:37 +0700

    Ing Commit v0.1 16:15

---
+++ FontEnd/NamKUB-Store/src/app/UserSite/user-login/user-login.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrl: './user-login.component.css'
+})
+export class UserLoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+  // fetch data 
+  onLogin(): void {
+    if (!this.username || !this.password) {
+      this.errorMessage = 'Username and password are required';
+      return;
+    }
+
+    const loginData = { username: this.username, password: this.password };
+
+    this.http.post<{ message: string }>('http://localhost:3000/api/login', loginData)
+      .subscribe({
+        next: (response) => {
+          console.log(response.message);
+          this.router.navigate(['/']); // Navigate to the home page after successful login
+        },
+        error: (error) => {
+          if (error.status === 401) {
+            this.errorMessage = 'Invalid username or password';
+          } else {
+            this.errorMessage = 'Server error';
+          }
+        }
+      });
+  }
+}
--- FontEnd/NamKUB-Store/src/app/app-routing.module.ts
+++ FontEnd/NamKUB-Store/src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserLoginComponent } from './User/user-login/user-login.component';
-import { UserRegComponent } from './User/user-reg/user-reg.component';
-import { AdminLoginComponent } from './Admin/admin-login/admin-login.component';
-import { HomeComponent } from './home/home.component';
+import { AdminLoginComponent } from './AdminSite/admin-login/admin-login.component';
+import { AboutUsComponent } from './UserSite/about-us/about-us.component';
+import { ContactComponent } from './UserSite/contact/contact.component';
+import { HomeComponent } from './UserSite/home/home.component';
+import { ProductComponent } from './UserSite/product/product.component';
+import { UserLoginComponent } from './UserSite/user-login/user-login.component';
+import { UserRegComponent } from './UserSite/user-reg/user-reg.component';
+
 
 const routes: Routes = [
   {path: '',component: UserLoginComponent},
   {path: 'register',component: UserRegComponent},
   {path: 'AdminRegister',component: AdminLoginComponent},
+  {path: 'home',component:HomeComponent},
+  {path: 'products',component:ProductComponent},
+  {path: 'contact',component:ContactComponent},
+  {path: 'about-us',component:AboutUsComponent},
   {path: 'home',component:HomeComponent}
 ];
 
