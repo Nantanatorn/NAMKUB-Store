a8fe14494fc4f6ef9257145c00556924b6ac7f5f
poppy <poppypopcorn@gmail.com>
2024-09-16 19:36:39 +0700

    fix conflict

--- FontEnd/NamKUB-Store/src/app/app.module.ts
+++ FontEnd/NamKUB-Store/src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-<<<<<<< HEAD
 
-=======
->>>>>>> main
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
